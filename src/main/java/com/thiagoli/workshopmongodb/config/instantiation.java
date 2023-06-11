2fc785f4b0219d42c8f869ddef862b701e68266e
thiago771414 <thiso2@hotmail.com>
2023-06-11 16:38:37 -0300

    Referenciando os posts do usuário

--- src/main/java/com/thiagoli/workshopmongodb/config/instantiation.java
+++ src/main/java/com/thiagoli/workshopmongodb/config/instantiation.java
@@ -44,5 +44,8 @@ public class instantiation implements CommandLineRunner{
 		Post post2 = new Post(null, sdf.parse("10/06/2023"), "Bom Dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 }
